7fb3c1deb4aa4f4a31c2f6eec05c57fbff259070
DESKTOP-NU25MII\grass <grass527@gmail.com>
2018-02-20 23:21:19 +0800

    create vs console project & basic code

---
+++ BMICalculator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMICalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Height(m):");
+            string height = Console.ReadLine();
+
+            Console.Write("Weight(kg):");
+            string weight = Console.ReadLine();
+
+            float bmi = float.Parse(weight) / (float.Parse(height) * float.Parse(height));
+            
+            Console.WriteLine("BMI: "+bmi.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # BMI-Calculator
+This is a basic BMI Calculator app written in C#. It's a demonstration for C# beginners.
+It's also a project from DMA Basic Programming Course.
